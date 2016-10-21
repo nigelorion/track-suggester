5bc6d4e9423bb69bd54c2d6ad548f176b4639468
Nigel Burnett <nigelorion@gmail.com>
2016-10-21 14:14:35 -0700

    Add content to fill lorem ipsum

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,42 @@
 var result;
 
 var trackSuggester = function(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection) {
-        if (q1Selection === "yes") {
-          return "ruby";
-      }
-      else if (q1selection === "no" && q5Selection === "yes") {
-        return "php";
 
-      }
-      else if (q3Selection) {
-        return "java";
+  if (q1Selection === "Yes") {
+  return "net";
+}
 
-      }
-      else if (q5Selection === "no") {
-        return "css";
-
-      }
-      else {
-        return "net";
-      }
-
-
-
-
-
-
+else if (q3Selection === "No") {
+  return "java"
 }
 
 
+else {
+  return "css";
+}
+}
 
-
-
-
+//Everything below is logic, everything above is business.
 
 $(document).ready(function() {
 
-  $("form").submit(function(event) {
-    var q1Selection = $("select#q1").val()
-    var q2Selection = $("select#q2").val()
-    var q3Selection = $("select#q3").val()
-    var q4Selection = $("select#q4").val()
-    var q5Selection = $("select#q5").val()
-
-    $("#" + result).hide();
-
-    var result = trackSuggester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
+$("form").submit(function(event) {
 
 
-       $("#" + result).fadeIn();
+ var q1Selection = $("select#q1").val();
+ var q2Selection = $("select#q2").val();
+ var q3Selection = $("select#q3").val();
+ var q4Selection = $("select#q4").val();
+ var q5Selection = $("select#q5").val();
+
+     $("#" + result).hide();
+
+     result = trackSuggester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
+
+     $("#" + result).fadeIn(800);
 
 
-    event.preventDefault();
-  });
-
-//user later to click next hide?
-//   $("#next").click(function(){
-//     var current = $(".name:visible");
-//     current.hide();
-//     current.next().show();
-// });
+  event.preventDefault();
+});
 
 });
