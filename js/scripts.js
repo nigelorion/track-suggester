1981fd0641a3ace11e27179ded01d5c2f98b8661
Nigel Burnett <nigelorion@gmail.com>
2016-10-21 12:13:44 -0700

    Add selector id tags, build structure of trackSuggester

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 var result;
 
-var trackSugguester = function(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection) {
+var trackSuggester = function(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection) {
+        if (q1Selection === "yes") {
+          return "ruby";
+      }
+      else if (q1selection === "no" && q5Selection === "yes") {
+        return "php";
 
+      }
+      else if (q3Selection) {
+        return "java";
+
+      }
+      else if (q5Selection === "no") {
+        return "css";
+
+      }
+      else {
+        return "net";
+      }
 
 
 
@@ -18,21 +35,22 @@ var trackSugguester = function(q1Selection, q2Selection, q3Selection, q4Selectio
 
 $(document).ready(function() {
 
-  $("form#survey").submit(function(event) {
-    var q1Selection = $("input:radio[name=option]:checked").val()
-    var q2Selection = $("input:radio[name=option]:checked").val()
-    var q3Selection = $("input:radio[name=option]:checked").val()
-    var q4Selection = $("input:radio[name=option]:checked").val()
-    var q5Selection = $("input:radio[name=option]:checked").val()
+  $("form").submit(function(event) {
+    var q1Selection = $("select#q1").val()
+    var q2Selection = $("select#q2").val()
+    var q3Selection = $("select#q3").val()
+    var q4Selection = $("select#q4").val()
+    var q5Selection = $("select#q5").val()
 
     $("#" + result).hide();
 
-    var result = trackSugguester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
-
-    $("#" + result).show();
+    var result = trackSuggester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
 
 
-    event.preventDefualt();
+       $("#" + result).fadeIn();
+
+
+    event.preventDefault();
   });
 
 //user later to click next hide?
