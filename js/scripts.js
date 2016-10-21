cc770310fd4fb93c70d9c16a25a3fb9cb5fba012
Nigel Burnett <nigelorion@gmail.com>
2016-10-21 15:07:07 -0700

    Add custom CSS styling, and finalize JQuery logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,41 +2,47 @@ var result;
 
 var trackSuggester = function(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection) {
 
-  if (q1Selection === "Yes") {
+  if (q5Selection === "Yes") {
+  return "java";
+}
+
+else if (q1Selection === "Yes" && q2Selection === "Yes") {
   return "net";
 }
 
-else if (q3Selection === "No") {
-  return "java"
-}
-
-
-else {
+else if (q3Selection === "Yes") {
   return "css";
 }
+
+else if (q2Selection === "Yes" && q4Selection === "Yes") {
+  return "ruby";
 }
 
-//Everything below is logic, everything above is business.
+else {
+  return "php";
+}
+}
+
+//_________________________________________
 
 $(document).ready(function() {
 
-$("form").submit(function(event) {
+$("form#survey").submit(function(event) {
 
+  var q1Selection = $("select#q1").val();
+  var q2Selection = $("select#q2").val();
+  var q3Selection = $("select#q3").val();
+  var q4Selection = $("select#q4").val();
+  var q5Selection = $("select#q5").val();
 
- var q1Selection = $("select#q1").val();
- var q2Selection = $("select#q2").val();
- var q3Selection = $("select#q3").val();
- var q4Selection = $("select#q4").val();
- var q5Selection = $("select#q5").val();
+  $("#" + result).hide();
 
-     $("#" + result).hide();
+  result = trackSuggester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
 
-     result = trackSuggester(q1Selection, q2Selection, q3Selection, q4Selection, q5Selection);
+  $("#" + result).fadeIn(800);
 
-     $("#" + result).fadeIn(800);
+event.preventDefault();
 
-
-  event.preventDefault();
 });
 
 });
